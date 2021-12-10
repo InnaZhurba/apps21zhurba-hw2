69b0ed4b92e99d486343569a2c579f68209a1c2b
innazhurba <inazhurba@gmail.com>
2021-12-10 23:09:13 +0200

    implement Queue and tests

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,15 +1,31 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+import ua.edu.ucu.collections.immutable.ImmutableList;
+
 public class Queue {
+    ImmutableList list = new ImmutableLinkedList();
 
     public Object peek() {
-        return null;
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        return ((ImmutableLinkedList)list).getFirst();
     }
 
     public Object dequeue() {
-        return null;
+        if (list.isEmpty()) {
+            return null;
+        }
+
+        Object first = ((ImmutableLinkedList)list).getFirst();
+        list = ((ImmutableLinkedList) list).removeFirst();
+
+        return first;
     }
 
     public void enqueue(Object e) {
+        list = list.add(e);
     }
 }
